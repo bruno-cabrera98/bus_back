80b02724b29b3123fc2492163bd453004be668b4
bruno.cabrera98 <bruno.cabrera98@gmail.com>
2022-08-29 23:22:13 -0300

    Add bus marker numbers, add tailwind

--- routes/bus_list.js
+++ routes/bus_list.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/list', async function(req, res, next) {
-    res.json(global.bus_data)
+    const bus_list = req.query.busList
+    if (bus_list)
+    {
+        const parsed_bus_list = bus_list.split(',')
+        const lines = global.bus_data.filter(bus => parsed_bus_list.includes(bus.line))
+        res.json(lines)
+    }
+    else
+    {
+        res.json(global.bus_data)
+    }
 })
 
 module.exports = router
